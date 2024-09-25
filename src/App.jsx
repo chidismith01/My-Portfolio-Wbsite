9413dceb259d69f58db30b55cc1466e57c81f94c
Emereuwa Chidiebere <108008810+chidismith01@users.noreply.github.com>
2024-09-25 19:16:47 +0000

    Set my portfolio site Github Pages

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Chidi Smith",
+  title: "Brand Expert & Web Deesigner",
+  email: "chidismith01@gmail.com",
+  gitHub: "chidismith01",
+  instagram: "https://www.instagram.com/4cuzstudios/",
+  linkedIn: "https://www.linkedin.com/in/emereuwachidiebere/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "https://x.com/ebyeast",
+  youTube: "https://www.youtube.com/channel/UCfuiTTIhJbk00ZK4Slr_oxg",
 };
 
 const primaryColor = "#4E567E";
